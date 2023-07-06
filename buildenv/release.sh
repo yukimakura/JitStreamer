a271993c0912f7f45978e44469df913dd1510db8
yukimakura <yukimakura86@gmail.com>
2023-07-06 22:43:46 +0900

    temp

--- buildenv/release.sh
+++ buildenv/release.sh
@@ -1,7 +1,7 @@
-docker build --build-arg IMAGE=arm32v7/ubuntu:21.10 --output . .
+# docker build --build-arg IMAGE=arm32v7/ubuntu:21.10 --output . .
 
 # docker build --build-arg IMAGE=arm64v8/alpine:3.15 .
 
-# docker build --build-arg IMAGE=amd64/alpine:3.15 .
+docker build --build-arg IMAGE=amd64/ubuntu:22.04 .
 
 # docker build --build-arg IMAGE=i386/alpine:3.15 .
